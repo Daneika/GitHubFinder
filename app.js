529fca3968c602bf6f785162a1753c64ffa568a5
meg <megan.daneika@gmail.com>
2020-05-21 09:27:59 +0100

    Add functionality for fetching and displaying repos & updating authorization

--- app.js
+++ app.js
@@ -13,12 +13,14 @@ searchUser.addEventListener("keyup", (e) => {
   if (userText !== "") {
     // Make http call
     github.getUser(userText).then((data) => {
+        console.log(data)
       if (data.profile.message === "Not Found") {
           ui.showAlert("User not found", "alert alert-danger")
       } else {
         ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
       }
-    });
+    }).catch(err => console.log(err));
   } else {
       ui.clearProfile();
   }
--- github.js
+++ github.js
@@ -2,17 +2,38 @@ class Github {
   constructor() {
     this.client_id = "481c45f414ab2e11c1fe";
     this.client_secret = "1d39cf63ae184f9750876fce73d23202a0bc91a0";
+    this.repos_count = 5;
+    this.repos_sort = "created";
+    this.repos_direction = "asc";
   }
 
   async getUser(user) {
+    console.log(user);
     const profileResponse = await fetch(
-      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+      new Request(`https://api.github.com/users/${user}`, {
+        headers: {
+          "Authorization": `Basic ${btoa(`${this.client_id}:${this.client_secret}`)}`
+        },
+      })
+    );
+
+    const repoResponse = await fetch(
+      new Request(
+        `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&direction=${this.repos_direction}`,
+        {
+          headers: {
+            "Authorization": `Basic ${btoa(`${this.client_id}:${this.client_secret}`)}`,
+          },
+        }
+      )
     );
 
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-        profile
-    }
+      profile,
+      repos,
+    };
   }
 }
